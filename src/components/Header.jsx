1e0d25015305de8569b4f4070a4498a44c6a1e7b
Mohammed-Nazleem <nazleem18@gmail.com>
2024-02-25 12:25:41 +0530

    Updated the project header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
   return (
     <header id='header'>
         <img src={imgLogo} alt="Logo showing a money bag" />
-       <h1>React Investment Calculator</h1>
+       <h1>Investment Calculator</h1>
     </header>
   )
 }
